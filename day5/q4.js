1441bdc3fec20fabfb4badbe046ab21f4b44e80c
Deepika-bethi <23eg104d16@anurag.edu.in>
2025-11-24 19:58:45 +0530

    Add q4.js with greeting and full name functions

---
+++ day5/q4.js
@@ -0,0 +1,20 @@
+// (a)
+const username = "John";
+const course = "JavaScript";
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+
+// (b)
+const name = "Sam";
+const age = 21;
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+
+// (c)
+const getFullName = (first, last) => `${first} ${last}`;
+
+console.log(getFullName("Sam", "Wilson"));
