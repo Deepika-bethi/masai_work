c8ff256c45f053a7f11b395af88d6f417daa09c8
Deepika-bethi <23eg104d16@anurag.edu.in>
2025-11-30 23:13:57 +0530

    Create script.js

---
+++ day10/q11.js/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("title").textContent = "Welcome to the DOM World!
+let paras = document.getElementsByTagName("p");
+for (let p of paras) {
+    p.style.color = "blue";
+}
+
+let box = document.querySelector(".container");
+box.style.backgroundColor = "yellow";
